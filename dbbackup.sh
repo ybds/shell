474b4a6b3364e10c0047492d9344eb730dd0e49b
ybds <38704865+ybds@users.noreply.github.com>
2019-11-26 17:23:01 +0800

    mysql backup
    mysql backup

---
+++ dbbackup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# author:LiZhenHua
+# date:2019-11-26
+
+date=`date +%Y%m%d%H%M%S`
+/usr/bin/mysqldump  -uroot web1>/backup/web1$date.data.sql
+/usr/bin/mysqldump  -uroot web2>/backup/web2$date.data.sql
+/usr/bin/mysqldump  -uroot web3>/backup/web3$date.data.sql
+
+zip -q -m web_$date.zip baseapi$date.data.sql eduoa$date.data.sql edures$date.data.sql
+
+find /backup/ -mtime 3 -name "*.zip" -exec rm -rf {} \;
+
+scp /backup/web_$date.zip backup@192.168.0.100:/dbbackup/web/
